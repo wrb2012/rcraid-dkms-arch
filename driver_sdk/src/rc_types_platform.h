61f62b7fd4f9c848a28dcc67b40d3a11142fd7da
wrb2012 <wurongbo2012@hotmail.com>
2025-05-29 21:44:25 +0800

    fix for gcc15

--- driver_sdk/src/rc_types_platform.h
+++ driver_sdk/src/rc_types_platform.h
@@ -29,7 +29,7 @@ typedef unsigned int		rc_uint32_t;
 typedef int			rc_int32_t;
 
 #ifndef __LINUX__
-typedef unsigned __int64 rc_uint64_t;
+typedef unsigned long rc_uint64_t;
 #else
 typedef unsigned long long  rc_uint64_t;
 #endif
